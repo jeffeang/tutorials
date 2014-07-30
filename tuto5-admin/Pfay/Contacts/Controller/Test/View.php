30d6d670f10b4b2e76a3f8567fc88629348aa2c9
pierrefay <pierrefay@gmail.com>
2014-07-31 00:36:23 +0200

    Sources du tutoriel Créer une interface d'admin sous magento2 http://www.pierrefay.fr/creer-une-inte…-sous-magento2-1193

---
+++ tuto5-admin/Pfay/Contacts/Controller/Test/View.php
@@ -0,0 +1,10 @@
+<?php
+namespace Pfay\Contacts\Controller\Test;
+
+class View extends \Magento\Framework\App\Action\Action
+{
+    public function execute()
+    {
+       die('test view');
+    }
+}

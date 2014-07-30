235704a9e700d1cbf9c70616412c0168e5467555
pierrefay <pierrefay@gmail.com>
2014-07-30 20:57:22 +0200

    Fichiers du tutoriel
    Installer Magento2
    http://www.pierrefay.fr/installer-magento2-1142

---
+++ tuto1-controleur/Pfay/Contacts/Controller/Test/Index.php
@@ -0,0 +1,10 @@
+<?php
+namespace Pfay\Contacts\Controller\Test;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    public function execute()
+    {
+       die('test index');
+    }
+}
